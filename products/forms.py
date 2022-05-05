2dbfc304fdb3015af5f50f12c21b12e251863573
sangkips <sangkipkoech@gmail.com>
2022-05-05 22:06:17 +0300

    added forms.py file

---
+++ products/forms.py
@@ -0,0 +1,12 @@
+from .models import Product
+from django import forms
+from django.forms import ModelForm
+
+
+class ProductForm(ModelForm):
+    class Meta:
+
+        model = Product
+
+        fields = ' __all__'
+
