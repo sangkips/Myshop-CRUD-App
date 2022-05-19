89965df060393a7e92dd83078cbd146f73aa3b81
sangkips <sangkipkoech@gmail.com>
2022-05-19 13:54:05 +0300

    created a logout page

--- products/urls.py
+++ products/urls.py
@@ -5,13 +5,15 @@ from .views import home_view, \
     update_product, \
     delete_product, \
     login_view, \
-    register_view
+    register_view, \
+    logout_view
 
 urlpatterns = [
-    path('home/', home_view, name='home'),
+    path('', home_view, name='home'),
 
-    path('', login_view, name='login'),
+    path('login', login_view, name='login'),
     path('register/', register_view, name='register-user'),
+    path('logout/', logout_view, name='logout'),
 
     path('create/', create_product, name='create-product'),
     path('update//<str:pk>', update_product, name='update-product'),
--- products/views.py
+++ products/views.py
@@ -1,11 +1,13 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .forms import ProductForm
 from .models import Product
 
 
+@login_required(login_url='login')
 def home_view(request):
     products = Product.objects.all()
     context = {
@@ -15,39 +17,51 @@ def home_view(request):
 
 
 def register_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            email = request.POST['email']
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
 
-        myuser = User.objects.create_user(username, email, password1)
-        myuser.first_name = fname
-        myuser.last_name = lname
+            myuser = User.objects.create_user(username, email, password1)
+            myuser.first_name = fname
+            myuser.last_name = lname
 
-        myuser.save()
+            myuser.save()
 
-        messages.success(request, f"Your account has been created successfully")
-        return redirect('login')
-    return render(request, 'products/register.html')
+            messages.success(request, f"Your account has been created successfully")
+            return redirect('login')
+        return render(request, 'products/register.html')
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            return render(request, 'products/login.html', {'error_message': 'Incorrect username or password.'})
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
-        return render(request, 'products/login.html')
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'products/login.html', {'error_message': 'Incorrect username or password.'})
+        else:
+            return render(request, 'products/login.html')
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def create_product(request):
     form = ProductForm()
 
@@ -62,6 +76,7 @@ def create_product(request):
     return render(request, 'products/create.html', context)
 
 
+@login_required(login_url='login')
 def update_product(request, pk):
     products = Product.objects.get(id=pk)
     form = ProductForm(instance=products)
@@ -78,6 +93,7 @@ def update_product(request, pk):
     return render(request, 'products/update.html', context)
 
 
+@login_required(login_url='login')
 def delete_product(request, pk):
     products = Product.objects.get(id=pk)
 
